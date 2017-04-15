dcc5e70cea3232d6715ab0ba81125c51afc4fa6d
Aleksey-Solovyev <Aleksey-Solovyev@users.noreply.github.com>
2017-04-16 00:52:00 +0300

    Finished ChatViewController

--- P2PChatApp/ChatViewController.m
+++ P2PChatApp/ChatViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "ChatViewController.h"
-
+/////
+#import "AppDelegate.h"
+/////
 @interface ChatViewController ()
+/////
+@property (nonatomic, strong) AppDelegate *appDelegate;
 
+-(void)sendMyMessage;
+
+-(void)didReceiveDataWithNotification:(NSNotification *)notification;
+/////
 @end
 
 @implementation ChatViewController
@@ -17,7 +25,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    /////
+    _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    /////
+    _textMessage.delegate = self;
+    /////
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didReceiveDataWithNotification:) name:@"MCDidReceiveDataNotification" object:nil];
 }
+
+/////
+-(void) sendMyMessage{
+    NSData *dataToSend = [_textMessage.text dataUsingEncoding:NSUTF8StringEncoding];
+    NSArray *allPeers = _appDelegate.p2pConnector.session.connectedPeers;
+    NSError *error;
+    
+    [_appDelegate.p2pConnector.session sendData:dataToSend toPeers:allPeers withMode:MCSessionSendDataReliable error:&error];
+    
+    if (error) {
+        NSLog(@"%@", [error localizedDescription]);
+    }
+    
+    [_textChat setText:[_textChat.text stringByAppendingString:[NSString stringWithFormat:@"Me:\n%@\n\n", _textMessage.text]]];
+    [_textMessage setText:@""];
+    [_textMessage resignFirstResponder];
+}
+/////
+-(void)didReceiveDataWithNotification:(NSNotification *)notification{
+    MCPeerID *peerID = [[notification userInfo] objectForKey:@"peerID"];
+    NSString *peerDisplayName = peerID.displayName;
+    
+    NSData *receivedData = [[notification userInfo] objectForKey:@"data"];
+    NSString *receivedText = [[NSString alloc] initWithData:receivedData encoding:NSUTF8StringEncoding];
+    
+    [_textChat performSelectorOnMainThread:@selector(setText:) withObject:[_textChat.text stringByAppendingString:[NSString stringWithFormat:@"%@ wrote: \n%@\n\n", peerDisplayName, receivedText]] waitUntilDone:NO];
+}
+/////
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self sendMyMessage];
+    return YES;
+}
+/////
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -35,8 +82,14 @@
 */
 
 - (IBAction)sendMessage:(id)sender {
+    /////
+    [self sendMyMessage];
+    /////
 }
 
 - (IBAction)cancelMessage:(id)sender {
+    /////
+    [_textMessage resignFirstResponder];
+    /////
 }
 @end

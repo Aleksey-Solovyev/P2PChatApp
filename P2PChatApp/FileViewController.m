69274768f55e48e46ae69155fbbb9d9cea02c9a1
Aleksey Solovyev <macbook@Device-MacBook.local>
2017-04-19 18:11:33 +0300

    Sample files for file share testing

--- P2PChatApp/FileViewController.m
+++ P2PChatApp/FileViewController.m
@@ -16,6 +16,8 @@
 @property (nonatomic, strong) AppDelegate *appDelegate;
 @property (nonatomic, strong) NSString *documentsDirectory;
 -(void) copySampleFilesToDocDirIfNeeded;
+@property (nonatomic, strong) NSMutableArray *arrFiles;
+-(NSArray *) getAllDocDirFiles;
 /////
 @end
 
@@ -27,8 +29,92 @@
     
     /////
     _appDelegate = (AppDelegate *) [[UIApplication sharedApplication] delegate];
+    /////
+    [self copySampleFilesToDocDirIfNeeded];
+    /////
+    _arrFiles = [[NSMutableArray alloc] initWithArray:[self getAllDocDirFiles]];
+    [_tableFiles setDelegate:self];
+    [_tableFiles setDataSource:self];
+    [_tableFiles reloadData];
+}
+/////
+-(void)copySampleFilesToDocDirIfNeeded{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    _documentsDirectory = [[NSString alloc] initWithString:[paths objectAtIndex:0]];
+    
+    NSString *file1Path = [_documentsDirectory stringByAppendingPathComponent:@"sample1.txt"];
+    NSString *file2Path = [_documentsDirectory stringByAppendingPathComponent:@"sample2.txt"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error;
+    
+    
+    if (![fileManager fileExistsAtPath:file1Path] || ![fileManager fileExistsAtPath:file2Path]) {
+        [fileManager copyItemAtPath:[[NSBundle mainBundle] pathForResource:@"sample1" ofType:@"txt"]
+                             toPath:file1Path
+                              error:&error];
+        
+        if (error) {
+            NSLog(@"%@", [error localizedDescription]);
+            return;
+        }
+        
+        [fileManager copyItemAtPath:[[NSBundle mainBundle] pathForResource:@"sample2" ofType:@"txt"]
+                             toPath:file2Path
+                              error:&error];
+        
+        if (error) {
+            NSLog(@"%@", [error localizedDescription]);
+            return;
+        }
+    }
+}
+/////
+-(NSArray *)getAllDocDirFiles{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error;
+    NSArray *allFiles = [fileManager contentsOfDirectoryAtPath:_documentsDirectory error:&error];
+    
+    if (error) {
+        NSLog(@"%@", [error localizedDescription]);
+        return nil;
+    }
+    
+    return allFiles;
+}
+/////
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
 }
 
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_arrFiles count];
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:@"CellIdentifier"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CellIdentifier"];
+        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+    }
+    
+    cell.textLabel.text = [_arrFiles objectAtIndex:indexPath.row];
+    
+    [[cell textLabel] setFont:[UIFont systemFontOfSize:14.0]];
+    
+    return cell;
+}
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60.0;
+}
+/////
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
